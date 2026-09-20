4e895bbc464c08257d0668b7e548d0edd196dc54
HODL KONG 64 <hodlkong64@gmail.com>
2026-09-20 04:27:04 +0100

    fix: show SPARKY clip in active chat

--- frontend/src/components/WorkspaceChat/ChatContainer/index.jsx
+++ frontend/src/components/WorkspaceChat/ChatContainer/index.jsx
@@ -557,6 +557,7 @@ export default function ChatContainer({
               </div>
             </div>
           </DnDFileUploaderWrapper>
+          <SparkyFloatingClip />
           <ChatTooltips />
         </div>
         <SourcesSidebar />
