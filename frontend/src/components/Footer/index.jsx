503c05c655e1b392c604eebd0f12a5524453f046
HODL KONG 64 <hodlkong64@gmail.com>
2026-09-20 19:40:22 +0100

    fix: use uncached HODL Warriors footer asset

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -21,7 +21,7 @@ export const MAX_ICONS = 3;
 const SWARMSY_GITHUB_URL = "https://github.com/Crypto-Moonboys/SWARMSY-Ai";
 const HODL_WARRIORS_CHAT_URL = "https://t.me/gkniftyheads/46556";
 const HODL_WARRIORS_BUTTON_URL =
-  "https://raw.githubusercontent.com/Crypto-Moonboys/SWARMSY-Ai/master/images/HODL%20WARRIORS%20BUTTON.png";
+  "https://raw.githubusercontent.com/Crypto-Moonboys/SWARMSY-Ai/master/images/hodl-warriors-footer-button.png";
 
 export const ICON_COMPONENTS = {
   BookOpen: BookOpen,
